a3a7e8213eb2c5fb3d74a21981ed8e16cc1ce90a
guopeiqing <peiqing.guo@mail.hypers.com>
2021-08-11 10:55:42 +0800

    feat：修改目录层级

--- daily_demo/日常/MongoORM/TEST.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import *
-
-print(connect('tumblelog'))
-
-
-class User(Document):
-    email = StringField(required=True)
-    first_name = StringField(max_length=50)
-    last_name = StringField(max_length=50)
